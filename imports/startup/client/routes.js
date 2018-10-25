402bdb4f19bfcdef9c3cf54cfb4e495d38a5ca8a
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-10-25 19:05:49 +0200

    [Fix]

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { render } from 'react-dom';
 
 // route components
-import Home from '../../ui/pages/Home/Home.js';
+import App from '../../ui/pages/Home/App.js';
 import LogIn from '../../ui/pages/LogIn/LogIn.js';
 import SignUp from '../../ui/pages/SignUp/SignUp.js';
-import MyAccount from '../../ui/pages/MyAccount/MyAccount.js';
+import Dashboard from '../../ui/pages/Dashboard/Dashboard.js'
+import NavBar from '../../ui/pages/Dashboard/Navbar.js'
+import Board from '../../ui/pages/Home/BoardDisplay.js'
 
 Meteor.startup(() => {
+    render(NavBar,document.getElementById('navbar'))
     render(
         <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
+            <div>
+                <Route exact path="/" component={App} />
                 <Route path="/login" component={LogIn}/>
                 <Route path="/signup" component={SignUp}/>
-                <Route path="/myaccount" component={MyAccount}/>
-            </Switch>
+                <Route path='/dashboard' component={Dashboard}/>
+                <Route path='/board/:id' component={Board}/>
+            </div>
         </Router>,
+
         document.getElementById('render-target')
     )
+
+
 })
