f76104598e5edfe8a1fc8db97498d480ae5920bf
granelj <joris.granel@etu.umontpellier.fr>
2018-10-30 10:42:07 +0100

    ADD: simple schema for right, notif, activity

---
+++ imports/startup/server/models/Right.js
@@ -0,0 +1,19 @@
+import { Mongo } from 'meteor/mongo'
+
+export const Rights = new Mongo.Collection('rights')
+
+import SimpleSchema from 'simpl-schema';
+
+RightSchema = new SimpleSchema({
+  RightId: {
+      type: String,
+      label: "Id",
+      regEx: SimpleSchema.RegEx.Id
+  },
+  RightName: {
+      type: String,
+      label: "Name",
+  }
+});
+
+Rights.attachSchema(RightSchema);
