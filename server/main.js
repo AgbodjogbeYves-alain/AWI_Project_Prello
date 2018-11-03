4d17fae9095181304784ed7455e2fea522abacb9
Unknown <fabien.turgut@etu.umontpellier.fr>
2018-11-03 23:28:31 +0100

    fix merge

--- server/main.js
+++ server/main.js
@@ -2,11 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 import './api/users.js';
 import './api/boards';
-<<<<<<< HEAD
-import './api/ListsRoutes'
-=======
 import './api/lists'
->>>>>>> backEndDev
 
 Meteor.startup(() => {
 
