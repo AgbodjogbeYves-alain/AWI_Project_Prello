4c07ae4123237e2ac14b23ec4d7c5e7ea71422cd
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-11-03 20:37:27 +0100

    [Changes on board model]

---
+++ server/api/boards.js
@@ -0,0 +1,47 @@
+import {Boards} from "../models/Boards";
+import {Meteor} from "meteor/meteor";
+
+Meteor.publish('boards', function () {return Boards.find()});
+
+Meteor.methods({
+    'board.createBoard'(boardName) {
+        return Boards.insert({boardTitle: boardName,boardPrivacy: 1, boardUser: [Meteor.user()]})
+    },
+
+    'board.getBoard' (idBoard) {
+        let board;
+        let countDoc = Boards.find({"boardId": idBoard}).count();
+        console.log(countDoc)
+        if (countDoc === 1) {
+            board = Boards.findOne({"boardId": idBoard});
+            return board;
+        } else {
+            throw new Meteor.Error(404, 'Board not found')
+        }
+
+    },
+
+    'boards.removeBoard'(boardId) {
+        return Boards.remove(boardId);
+    },
+
+    'boards.editBoard' (newBoard) {
+        return Boards.update({boardId: newBoard.boardId}, { $set: {
+                boardTitle: newBoard.boardTitle,
+                boardPrivacy: newBoard.privacy
+        }})
+
+    },
+
+    'getAllBoards' (){
+        return Boards.find().fetch();
+    }
+})
+
+
+Meteor.method("test-board", function (a, b) {
+    return a + b;
+}, {
+    url: "test/:id",
+    httpMethod: "post"
+})
