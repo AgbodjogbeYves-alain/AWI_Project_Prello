1f53e05267021b40f8a3096fd6b6f1e1433cc611
FlorentBerland <florent.berland@laposte.net>
2018-11-15 17:16:03 +0100

    Fix add comment

--- server/api/comments.js
+++ server/api/comments.js
@@ -2,57 +2,59 @@ import {Meteor} from "meteor/meteor";
 import {Random} from 'meteor/random';
 import {JsonRoutes} from 'meteor/simple:json-routes';
 import {Boards} from "../models/Boards";
+import {boardUtils} from "./Utils/boardUtils"
+import {canPerform,
+        EDIT_CARD
+} from "./Utils/roles"
 
 Meteor.methods({
     'boards.comment.addComment' (idBoard,idList,idCard, newComment) {
-        let countDoc = Boards.find({"_id": idBoard}).count();
-        let id = Random.id();
+        let userId = this.userId
+        let board = Boards.find({"_id": idBoard})
 
-        if(countDoc==1){
-            //List
-            let findList = false;
-            let findCard = false;
-            let board = (Boards.findOne({_id: idBoard}))
-            let newBoardLists = board.boardLists.map((list) => {
-                if(list._id == idList){
-                    findList = true
-                    list.listCard = list.listCard.map((card) => {
-                        if (card._id == idCard) {
-                            findCard = true
-                            let newCommentTab = card.cardComments
-                            let newCommentI = {_id: id, commentContent: newComment.commentContent, userId: newComment.userId}
-                            newCommentTab.push(newCommentI)
-                            card.cardComments = newCommentTab
-                            return card
-                        } else {
-                            return card
-                        }
-                    })
-                    return list
+        if(board){
+            let userRole = boardUtils.getUserRole(userId, board)
+            if(canPerform(userRole, EDIT_CARD)){
+                //List
+                let id = Random.id();
+                let findList = false;
+                let findCard = false;
+                let newBoardLists = board.boardLists.map((list) => {
+                    if(list._id == idList){
+                        findList = true
+                        list.listCard = list.listCard.map((card) => {
+                            if (card._id == idCard) {
+                                findCard = true
+                                let newCommentTab = card.cardComments
+                                let newCommentI = {_id: id, commentContent: newComment.commentContent, userId: newComment.userId}
+                                newCommentTab.push(newCommentI)
+                                card.cardComments = newCommentTab
+                                return card
+                            } else {
+                                return card
+                            }
+                        })
+                        return list
+                    }else{
+                        return list
+                    }
+                })
+
+                if(findList!=true){
+                    throw new Meteor.Error(404, 'List in Board not found')
+                }else if(findCard!=true) {
+                    throw new Meteor.Error(404, 'Card in Board not found')
                 }else{
-                    return list
-                }
-            })
-
-            if(findList!=true){
-                throw new Meteor.Error(404, 'List in Board not found')
-            }else if(findCard!=true) {
-                throw new Meteor.Error(404, 'Card in Board not found')
-            }else{
-
-                {
                     Boards.update({_id: board._id},{
                         $set: {
                             boardLists: newBoardLists
                         }
                     })
                 }
-            }
+            } else
+                throw new Meteor.Error(403, "You do not have the permission to comment a card")
         }else{
             throw new Meteor.Error(404, 'Board not found')
         }
-
-
     }
-
 })
