f914703ddd7791ad45ec80cbe6a4e9db1781e1e7
FlorentBerland <florent.berland@laposte.net>
2018-11-15 16:01:22 +0100

    Add rights management for cards

--- server/api/cards.js
+++ server/api/cards.js
@@ -4,7 +4,8 @@ import {Boards} from "../models/Boards";
 import {boardUtils} from "./Utils/boardUtils";
 import {canPerform,
         CREATE_CARD,
-        EDIT_CARD
+        EDIT_CARD,
+        DELETE_CARD
     } from "./Utils/roles";
 
 
@@ -102,46 +103,50 @@ Meteor.methods({
         let board = Boards.find({"_id": idBoard})
 
         if(board){
-            //List
-            let findList = false;
-            let findCard = false;
-            let id = Random.id();
+            let userRole = boardUtils.getUserRole(userId, board)
+            if(canPerform(userRole, EDIT_CARD)){
+                //List
+                let findList = false;
+                let findCard = false;
+                let id = Random.id();
 
-            let newComment  = newCard.cardComments[newCard.cardComments.length-1]
-            //let newCommentI = {_id: id, commentContent: newComment.commentContent, userId: newComment.userId}
-            newComment._id = id
-            newCard.cardComments[newCard.cardComments.length-1] = newComment
-            let newBoardLists = board.boardLists.map((list) => {
-                if(list._id == idList){
-                    findList = true
-                    list.listCards = list.listCards.map((card) => {
-                        if (card._id == newCard._id) {
-                            findCard = true
-                            return newCard
-                        } else {
-                            return card
-                        }
-                    })
-                    return list
+                let newComment  = newCard.cardComments[newCard.cardComments.length-1]
+                //let newCommentI = {_id: id, commentContent: newComment.commentContent, userId: newComment.userId}
+                newComment._id = id
+                newCard.cardComments[newCard.cardComments.length-1] = newComment
+                let newBoardLists = board.boardLists.map((list) => {
+                    if(list._id == idList){
+                        findList = true
+                        list.listCards = list.listCards.map((card) => {
+                            if (card._id == newCard._id) {
+                                findCard = true
+                                return newCard
+                            } else {
+                                return card
+                            }
+                        })
+                        return list
+                    }else{
+                        return list
+                    }
+                })
+
+                if(findList!=true){
+                    throw new Meteor.Error(404, 'List in Board not found')
+                }else if(findCard!=true) {
+                    throw new Meteor.Error(404, 'Card in Board not found')
                 }else{
-                    return list
-                }
-            })
 
-            if(findList!=true){
-                throw new Meteor.Error(404, 'List in Board not found')
-            }else if(findCard!=true) {
-                throw new Meteor.Error(404, 'Card in Board not found')
-            }else{
-
-                {
-                    Boards.update({_id: board._id},{
-                        $set: {
-                            boardLists: newBoardLists
-                        }
-                    })
+                    {
+                        Boards.update({_id: board._id},{
+                            $set: {
+                                boardLists: newBoardLists
+                            }
+                        })
+                    }
                 }
-            }
+            } else 
+                throw new Meteor.Error(403, "You do not have permission to comment a card")
         }else{
             throw new Meteor.Error(404, 'Board not found')
         }
@@ -149,33 +154,37 @@ Meteor.methods({
 
     },
     'boards.card.removeCard' (idBoard,idList,idCard){
-        let countDoc = Boards.find({"_id": idBoard}).count();
-        if(countDoc==1){
-            //List
-            let findList = false;
-            let board = (Boards.findOne({_id: idBoard}))
-            let newBoardLists = board.boardLists.map((list) => {
-                if(list._id == idList){
-                    findList = true
-                    list.listCards = list.listCards.filter((card) => card._id!=idCard)
-                    return list
+        let userId = this.userId
+        let board = Boards.findOne({"_id": idBoard})
+        if(board){
+            let userRole = boardUtils.getUserRole(userId, board)
+            if(canPerform(userRole, DELETE_CARD)){
+                //List
+                let findList = false;
+                let newBoardLists = board.boardLists.map((list) => {
+                    if(list._id == idList){
+                        findList = true
+                        list.listCards = list.listCards.filter((card) => card._id!=idCard)
+                        return list
+                    }else{
+                        return list
+                    }
+                })
+
+                if(findList!=true){
+                    throw new Meteor.Error(404, 'List in Board not found')
                 }else{
-                    return list
-                }
-            })
 
-            if(findList!=true){
-                throw new Meteor.Error(404, 'List in Board not found')
-            }else{
-
-                {
-                    Boards.update({_id: board._id},{
-                        $set: {
-                            boardLists: newBoardLists
-                        }
-                    })
+                    {
+                        Boards.update({_id: board._id},{
+                            $set: {
+                                boardLists: newBoardLists
+                            }
+                        })
+                    }
                 }
-            }
+            } else
+                throw new Meteor.Error(403, "You do not have permission to remove a card")
         }else{
             throw new Meteor.Error(404, 'Board not found')
         }
