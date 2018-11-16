0e7b1d41845b33f0829d7a9f760c4d31a5f09df9
FlorentBerland <florent.berland@laposte.net>
2018-11-16 02:03:41 +0100

    Add rights management for lists

--- server/api/boards.js
+++ server/api/boards.js
@@ -15,12 +15,15 @@ if(Meteor.isServer){
 
     Meteor.publish('boards', function () {
         let userId = this.userId;
+        /* Should manage accuarately the access rights but make the board creation bugged
         let boards = Boards.find({boardUsers : {$elemMatch: {'userId': userId}}})
         let ids = boards.fetch().filter(b => {
             let role = boardUtils.getUserRole(userId, b)
             return canPerform(role, ACCESS_BOARD)
         }).map(b => b._id)
         return Boards.find({_id : {$in: ids}})
+        */
+        return Boards.find({boardUsers : {$elemMatch: {'userId': userId}}})
     });
 
 }
